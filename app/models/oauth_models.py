f74374bf913a416d7ad69c9759d7333f40d52d21
Ayibatonye Ikemike <ayibatonyeikemike@users.noreply.github.com>
2025-11-22 14:13:23 +0100

    HOTFIX: Fix OAuthToken ForeignKey - use 'user' not 'users'

--- app/models/oauth_models.py
+++ app/models/oauth_models.py
@@ -50,7 +50,7 @@ class OAuthToken(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(
         Integer,
-        ForeignKey("users.id", ondelete="CASCADE"),
+        ForeignKey("user.id", ondelete="CASCADE"),
         nullable=False,
         index=True,
     )
