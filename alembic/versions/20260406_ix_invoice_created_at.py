80e70ebbee78e1ec3eb56f82cebe1cbd0b7fef9c
Ayibatonye Ikemike <ayibatonyeikemike@users.noreply.github.com>
2026-04-06 07:27:58 +0100

    fix: set correct down_revision for migration to fix deploy
    Migration had down_revision=None creating a second head,
    blocking alembic upgrade head on deploy.

--- alembic/versions/20260406_ix_invoice_created_at.py
+++ alembic/versions/20260406_ix_invoice_created_at.py
@@ -6,7 +6,7 @@ Create Date: 2026-04-06
 from alembic import op
 
 revision = "20260406_ix_invoice_created_at"
-down_revision = None
+down_revision = "add_testimonial_20260312"
 branch_labels = None
 depends_on = None
 
